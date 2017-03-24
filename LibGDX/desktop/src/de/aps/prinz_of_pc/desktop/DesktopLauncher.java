b0e6277885a775a1e240525506eee79dcec2e9b0
Patrick Domscheid <patrick.domscheid@googlemail.com>
2017-03-24 09:31:38 +0100

    Added LIbGDX (Library)

---
+++ LibGDX/desktop/src/de/aps/prinz_of_pc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.aps.prinz_of_pc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.aps.prinz_of_pc.PrinzGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PrinzGame(), config);
+	}
+}
---
+++ LibGDX/html/src/de/aps/prinz_of_pc/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package de.aps.prinz_of_pc.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.aps.prinz_of_pc.PrinzGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new PrinzGame();
+        }
+}
