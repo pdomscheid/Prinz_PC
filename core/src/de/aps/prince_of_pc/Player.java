1405c23689f534e779f897468e7cff88094eddfd
sabe111 <1512670@hs-mannheim.de>
2017-03-24 17:06:07 +0100

    Movement - ok Collision - nicht so

---
+++ core/src/de/aps/prince_of_pc/Player.java
@@ -0,0 +1,111 @@
+package de.aps.prince_of_pc;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+import com.badlogic.gdx.math.Vector2;
+
+public class Player extends Sprite {
+	
+	private float xPos = Gdx.graphics.getWidth()/2;
+	private float yPos = Gdx.graphics.getHeight()/2;
+	
+	private Vector2 movement = new Vector2(0,0);
+    private Texture character;
+    private TiledMapTileLayer collisionLayer;
+    
+
+	public Player (Sprite sprite, TiledMapTileLayer collisionLayer, float x, float y) {
+		super(sprite);
+		this.collisionLayer = collisionLayer;
+		this.xPos = x;
+		this.yPos = y;
+	}
+	
+	public float getxPos() {
+		return xPos;
+	}
+
+	public void setxPos(float xPos) {
+		this.xPos = xPos;
+	}
+
+	public float getyPos() {
+		return yPos;
+	}
+
+	public void setyPos(float yPos) {
+		this.yPos = yPos;
+	}
+	
+	public void update(){
+		boolean collisionX = false;
+		boolean collisionY = false;
+		
+		float oldX = getxPos();
+		float oldY = getyPos();
+		
+		if(getxPos() <  oldX) // going left
+			collisionX = collidesLeft();
+		else if(getxPos() > oldX) // going right
+			collisionX = collidesRight();
+		
+		// react to x collision
+				if(collisionX) {
+					setX(oldX);
+				} 
+				
+		if(getyPos() < oldY) // going down
+			collisionY = collidesBottom();
+				else if(getyPos() > oldY) // going up
+					collisionY = collidesTop();
+
+				// react to y collision
+				if(collisionY) {
+					setY(oldY);
+
+				}
+				
+				System.out.println(collisionX + " " + collisionY);
+				System.out.println(xPos + "  " + yPos);
+		
+	}
+	
+	private boolean isCellBlocked(float x, float y) {
+		Cell cell = collisionLayer.getCell((int) ( x / collisionLayer.getTileWidth()), (int)(y/collisionLayer.getTileHeight()));
+		
+		return cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey("1");
+	}
+	
+	public boolean collidesRight() {
+		for(float step = 0; step < getHeight(); step += collisionLayer.getTileHeight() / 2)
+			if(isCellBlocked(getX() + getWidth(), getY() + step))
+				return true;
+		return false;
+	}
+	
+	public boolean collidesLeft() {
+		for(float step = 0; step < getHeight(); step += collisionLayer.getTileHeight() / 2)
+			if(isCellBlocked(getX(), getY() + step))
+				return true;
+		return false;
+	}
+
+	public boolean collidesTop() {
+		for(float step = 0; step < getWidth(); step += collisionLayer.getTileWidth() / 2)
+			if(isCellBlocked(getX() + step, getY() + getHeight()))
+				return true;
+		return false;
+
+	}
+
+	public boolean collidesBottom() {
+		for(float step = 0; step < getWidth(); step += collisionLayer.getTileWidth() / 2)
+			if(isCellBlocked(getX() + step, getY()))
+				return true;
+		return false;
+	}
+
+}
--- core/src/de/aps/prince_of_pc/PrinceGame.java
+++ core/src/de/aps/prince_of_pc/PrinceGame.java
@@ -7,30 +7,26 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class PrinceGame extends ApplicationAdapter implements InputProcessor {
-    Texture img;
+   
+	Player player;
+	
+	Texture img;
     TiledMap tiledMap;
     OrthographicCamera camera;
     TiledMapRenderer tiledMapRenderer;
     private SpriteBatch batch;
-    private Texture charUp;
-    private Texture charDown;
-    private Texture charLeft;
-    private Texture charRight;
-    private Sprite spriteUp;
-    private Sprite spriteDown;
-    private Sprite spriteLeft;
-    private Sprite spriteRight;
+    private Texture character;
+    private Sprite playerSprite;
+
     
     private String lastState = "down";
     
@@ -41,28 +37,28 @@ public class PrinceGame extends ApplicationAdapter implements InputProcessor {
 		//Spielercharacter
     	batch = new SpriteBatch();
     	//Verschiedene Texturen für Richtungen
-    	charUp = new Texture(Gdx.files.internal("char_up.png"));
-    	charDown = new Texture(Gdx.files.internal("char_down.png"));
-    	charLeft = new Texture(Gdx.files.internal("char_left.png"));
-    	charRight = new Texture(Gdx.files.internal("char_right.png"));
-        spriteUp = new Sprite(charUp); 
-        spriteDown = new Sprite(charDown);
-        spriteLeft = new Sprite(charLeft);
-        spriteRight = new Sprite(charRight);
-
+    	character = new Texture(Gdx.files.internal("char_down.png"));
+    	
+        //Map
+        tiledMap = new TmxMapLoader().load("probemap.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+        
+      //Spielercharacter erstellen
+        playerSprite = new Sprite(character);
+        player = new Player(playerSprite, (TiledMapTileLayer) tiledMap.getLayers().get("blocked"),Gdx.graphics.getWidth() * 1f, Gdx.graphics.getHeight() * 4f);
+       
+        Gdx.input.setInputProcessor(this);
+       
+        
+        
         //Kamera
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
         camera = new OrthographicCamera(1280, 720);
         camera.setToOrtho(false,w,h);
-        camera.position.set(camera.viewportWidth / 4, camera.viewportHeight * 4f, 0);
-        camera.update();
-        
-        //Map
-        tiledMap = new TmxMapLoader().load("probemap.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-       
-        Gdx.input.setInputProcessor(this);
+        camera.position.set(player.getxPos(), player.getyPos(), 0);
+
+
         
        
     }
@@ -78,57 +74,42 @@ public class PrinceGame extends ApplicationAdapter implements InputProcessor {
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
+        player.update();
+        
         //Kamera u. Map rendern
         camera.update();
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
         
         //Characterposition
-        spriteUp.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-        spriteUp.setOrigin(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-        spriteDown.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-        spriteLeft.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-        spriteRight.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        player.setxPos(Gdx.graphics.getWidth()/2);
+        player.setyPos(Gdx.graphics.getHeight()/2);       
+        player.setPosition(player.getxPos(), player.getyPos());
+        
         
         //Movement
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-   		 camera.translate(-2f,0);
-   		 lastState = "left";
+   		 player.translate(-2f,0);
+   		 //lastState = "left";
    		 
         }
    		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-      		 camera.translate(2f,0);
-      		lastState = "right";
+      		 player.translate(2f,0);
+      		//lastState = "right";
    		}
    		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-	   		 camera.translate(0,2f);
-	   		lastState = "up";	
+	   		 player.translate(0,2f);
+	   		//lastState = "up";	
 	   		 }
    		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-	   		 camera.translate(0,-2f);	
-	   		lastState = "down";
+	   		 player.translate(0,-2f);	
+	   		//lastState = "down";
    		}
       	
       	
+   		 
       	 batch.begin();
-      	 //Kontrollstruktur für Anzeige der Charaktertextur
-         switch(lastState){
-         case "up": 
-        	 spriteUp.draw(batch);
-        	 break;
-         case "down":
-        	 spriteDown.draw(batch);
-        	 break;
-         case "left":
-        	 spriteLeft.draw(batch);
-        	 break;
-         case "right":
-        	 spriteRight.draw(batch);
-        	 break;
-         default:
-        	 spriteDown.draw(batch);
-        	 break;
-         }
+      	 player.draw(batch);
          batch.getProjectionMatrix().setToOrtho2D(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
          batch.end();
     }
